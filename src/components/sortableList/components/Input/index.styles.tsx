657aa51c7f2af4c733dd7c861c60845e204d7f42
Apple <happy3guy@gmail.com>
2023-10-08 17:37:54 +0200

    create InputComponent

---
+++ src/components/sortableList/components/Input/index.styles.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 224px;
+  position: relative;
+  margin-bottom: 10px;
+`;
+
+export const InputBox = styled.div`
+  input {
+    padding: 12px;
+    font-size: 16px;
+    border-radius: 8px;
+    background: #F7F7FA;
+    border: solid 1px #ECEDF5;
+  }
+  
+  input::placeholder {
+    color: #5F6591;
+  }
+`;
+
+export const DropdownList = styled.ul`
+  position: absolute;
+  width: 100%;
+  padding: 0;
+  padding-top: 8px;
+  margin: 0;
+  margin-top: 8px;
+  background: #F7F7FA;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  border-radius: 8px;
+  color: #5F6591;
+  font-size: 1.3rem;
+  font-weight: 500;
+`;
+
+export const ListItem = styled.li`
+  list-style: none;
+  padding: 0.4em;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #E4E4F0;
+  }
+`;
+
+export const ArrowIcon = styled.span`
+  position: absolute;
+  right: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
---
+++ src/components/sortableList/components/Input/index.tsx
@@ -0,0 +1,77 @@
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+
+import { ArrowDown } from "../../../../assets/icons/arrowDown";
+
+import {
+  InputContainer,
+  InputBox,
+  DropdownList,
+  ListItem,
+  ArrowIcon,
+} from "./index.styles";
+
+interface InputProps {
+  defaultSkillList: string[];
+  handleAddSkill: (value: string) => void;
+}
+
+export const Input: React.FC<InputProps> = (props) => {
+  const { defaultSkillList, handleAddSkill } = props;
+
+  const [value, setValue] = useState<string>("");
+  const filteredSkillList = defaultSkillList
+    .filter((skill) => skill.toLowerCase().startsWith(value.toLowerCase()))
+    .sort();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick, true);
+
+    return () => document.removeEventListener("click", handleClick, true);
+  }, [containerRef]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleItemSelected = (item: string) => {
+    setIsOpen(false);
+    handleAddSkill(item);
+    setValue("");
+  };
+
+  const handleFocus = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <InputContainer ref={containerRef}>
+      <InputBox>
+        <input value={value} onChange={handleChange} onFocus={handleFocus} placeholder="Add Skill" />
+        <ArrowIcon>
+          <ArrowDown />
+        </ArrowIcon>
+      </InputBox>
+      {!!filteredSkillList.length && !!value.length && isOpen && (
+        <DropdownList>
+          {filteredSkillList.map((item, key) => (
+            <ListItem key={key} onClick={() => handleItemSelected(item)}>
+              {item}
+            </ListItem>
+          ))}
+        </DropdownList>
+      )}
+    </InputContainer>
+  );
+};
